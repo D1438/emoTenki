78955d748d5521624d5dea460cf53d41710617b4
I.D <daisuke.workin@gmail.com>
2020-12-11 01:26:23 +0900

    initialize

---
+++ emoTenki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  emoTenki
+//
+//  Created by 石澤大輔 on 2020/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
